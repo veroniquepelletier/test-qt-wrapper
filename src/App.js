4ec35058e320fa5a386bdcefe3af164449733a44
veroniquepelletier <pelletierv@hotmail.com>
2018-10-29 20:38:09 -0400

    test
    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Qt } from './services/qt';
+
+const qt = new Qt();
 
 class App extends Component {
+
+  componentDidMount() {
+    this.sendEvent();
+  }
+
+  sendEvent() {
+    qt.sendEvent('toto', {value: 'hello'})
+    .then(results => {
+      console.log('results :' + results);
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
